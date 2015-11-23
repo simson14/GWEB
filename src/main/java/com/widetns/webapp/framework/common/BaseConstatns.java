fbfadfdab5039a094cfdbac8af03310742574fea
LJJ <simson14@widetns.com>
2015-11-23 18:05:52 +0900

    [GWEB] - 2015-11-23 - Depth형 로깅기능을 실행 Class의 이름으로 구분하였던 것을 실행 Class의 Annotation 타입으로 메시지 로깅방식을 변경함으로 불필요한 Constant들 삭제

--- src/main/java/com/widetns/webapp/framework/common/BaseConstatns.java
+++ src/main/java/com/widetns/webapp/framework/common/BaseConstatns.java
@@ -2,11 +2,7 @@ package com.widetns.webapp.framework.common;
 
 public interface BaseConstatns {
 
-    String CONTROLLER = "Controller";
-    String SERVICE = "Service";
-    String DAO = "Dao";
     String LOG_PREFIX = "--->";
-
     String PROFILE = "spring.profiles.active";
 
 }
